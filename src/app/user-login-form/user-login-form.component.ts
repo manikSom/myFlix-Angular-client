d93c62e7d1d36c6dfa84bed51d38ffa681ffb1f3
Manickam Somasundaram <manickam.som@gmail.com>
2023-05-10 18:15:50 +0200

    feat: user registration and login implemented

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss'],
+})
+export class UserLoginFormComponent {
+  @Input() userData = { Username: '', Password: '' };
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe(
+      (result) => {
+        console.log(result);
+        localStorage.setItem('username', result.user.userName);
+        localStorage.setItem('token', result.token);
+        this.dialogRef.close();
+        this.snackBar.open('Login successful', 'OK', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+}
